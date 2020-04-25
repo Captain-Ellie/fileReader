a37ca59f4e437aeb6a6bcbb5e3f00700c6a01541
Captain-Ellie <54179498+Captain-Ellie@users.noreply.github.com>
2020-04-25 15:44:00 +0300

    updated

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,38 +1,42 @@
 package com.company;
 import java.io.IOException;
-import java.util.Scanner;
 import java.io.*;
 
-public class Main
-{
-    public static void main(String[] args) throws IOException
-    {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("Enter the path to your folder: ");
-        String dir_path = reader.readLine() + "\\";
-        File folder = new File(dir_path);
-        File[] listOfFiles = folder.listFiles();
-        assert listOfFiles != null;
-        int count = listOfFiles.length;
-        String[] str_arr = new String[count];
-        for (int i = 0; i < count; i++) {
-            if (listOfFiles[i].isFile()) {
-                str_arr[i] = dir_path + listOfFiles[i].getName();
-            }
+public class Main {
 
-            Scanner scan = new Scanner(new FileInputStream(str_arr[i]));
-            int line_count = 0;
-            int words_count = 0;
-            while (scan.hasNextLine()) {
-                line_count++;
-                String[] array = scan.nextLine().split("\\s*(\\s|,|\\.!)\\s*");
-                words_count = words_count + array.length;
-            }
+    public static void main(String[] args) {
 
-            System.out.println("File: " + str_arr[i] + " - " + line_count + " lines" + " and " + words_count + " words");
+       try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+           System.out.print("Enter the path to your folder: ");
+           String dir_path = reader.readLine() + "\\";
+           File folder = new File(dir_path);
+           File[] listOfFiles = folder.listFiles();
+           assert listOfFiles != null;
+           int count = listOfFiles.length;
+           String[] str_arr = new String[count];
+           for (int i = 0; i < count; i++) {
+               if (listOfFiles[i].isFile()) {
+                   str_arr[i] = dir_path + listOfFiles[i].getName();
+               }
 
-            scan.close();
-        }
-        reader.close();
+               try (BufferedReader scan = new BufferedReader(new FileReader(str_arr[i]))) {
+                   int line_count = 0;
+                   int words_count = 0;
+                   FileReader fileReader = new FileReader(str_arr[i]);
+                   LineNumberReader lineNumReader = new LineNumberReader(fileReader);
+                   while ((lineNumReader.readLine()) != null) {
+                       line_count++;
+                       words_count += scan.readLine().split("\\s*(\\s|,|\\.!)\\s*").length;
+                   }
+                   System.out.println("File: " + str_arr[i] + " - " + line_count + " lines" + " and "
+                           + words_count + " words");
+
+               }catch (FileNotFoundException ex) {
+                   ex.printStackTrace();
+               }
+           }
+       }catch (IOException ex) {
+           ex.printStackTrace();
+       }
     }
 }
